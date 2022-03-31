0c24ee82e6161b9b25d0297efa053742838e68c6
HuangTao <huangtao23993@talkweb.com.cn>
2022-03-31 10:45:02 +0800

    修复命名

--- config/api.config.js
+++ config/api.config.js
@@ -6,7 +6,7 @@ const UPLOAD_URL = document.querySelector('body').getAttribute('upload_url');
 const config = {
   baseUrl: BASE_URL || '',
   logUrl: LOG_URL || 'http://121.37.19.86:10014',
-  oprationUrl: OPERATION_URL || 'http://121.37.19.86:10001',
+  operationUrl: OPERATION_URL || 'http://121.37.19.86:10001',
   imageUploadUrl: UPLOAD_URL || '/console/files/upload'
 };
 export default config;
