d52485d1f18e7555ea584265b60e1bf6b5889cb1
dingpeng24001 <dingpeng24001@talkweb.com.cn>
2022-04-11 14:08:25 +0800

    add ci config and scripts

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM nginx:latest
-COPY dist/  /usr/share/nginx/html/
+FROM --platform=$TARGETPLATFORM node:12 as builder
+WORKDIR /app
+COPY . .
+
+RUN yarn install && yarn run build
+
+FROM --platform=$TARGETPLATFORM  nginx:latest
+COPY --from=builder /app/dist/  /usr/share/nginx/html/
 
 CMD ["/bin/bash", "-c", "sed -i \"s@<html@<html base_url=\"$BASE_URL\" operation_url=\"$OPERATION_URL\" log_url=\"$LOG_URL\" upload_url=\"$UPLOAD_URL\" @\" /usr/share/nginx/html/index.html; nginx -g \"daemon off;\""]
---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# References:
+# 1. https://docs.docker.com/buildx/working-with-buildx/
+# 2. https://docs.docker.com/engine/reference/commandline/buildx/
+
+# Requirements:
+# 1. docker run --privileged --rm tonistiigi/binfmt --install all
+# 2. docker login dockerhub
+# 3. docker login myhuaweicloud-swr
+
+export NAMESPACE=wutong-ui
+export VERSION=v1.0.0-stable
+docker buildx create --use --name uibuilder
+# docker buildx use uibuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
+# docker buildx rm uibuilder
