a387f3143897b73c9736b883c6fe44e8c2b0ee3d
dingpeng24001 <dingpeng24001@talkweb.com.cn>
2022-04-11 16:24:09 +0800

    set multiarch release confid

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@
 export NAMESPACE=wutong-ui
 export VERSION=v1.0.0-stable
 docker buildx create --use --name uibuilder
-# docker buildx use uibuilder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile . 
-docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
-# docker buildx rm uibuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+docker buildx rm uibuilder
