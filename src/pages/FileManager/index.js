f592515bfad08a4eaf0a665f38739cf5b4862000
chengfeng23514 <chengfeng23514@talkweb.com.cn>
2022-05-25 17:04:06 +0800

    update filemanager url

--- src/pages/FileManager/index.js
+++ src/pages/FileManager/index.js
@@ -55,7 +55,7 @@ export default class FileManager extends PureComponent {
     return (
       <iframe
         // src={`/console/team/${teamName}/region/${regionName}/components/${appAlias}/filemanager/service_id/${serviceID}`}
-        src={`/filebrowser/${serviceID}`}
+        src={`/console/filebrowser/${serviceID}`}
         // src={`https://wt-api.wutong.talkweb.com.cn/filebrowser/${serviceID}/files`}
         style={{
           width: '100%',
