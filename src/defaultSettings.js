044aea42ebb56d0dde2b6c4c21074b714bd3d636
leon <liujinwen@anewayer.com>
2022-08-03 11:29:18 +0800

    fix: global primaryColor reset

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,6 +1,6 @@
 module.exports = {
   navTheme: 'light', // theme for nav menu
-  primaryColor: '#08153A', // primary color of ant design
+  primaryColor: '#3575CC', // primary color of ant design
   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
   fixedHeader: false, // sticky header
