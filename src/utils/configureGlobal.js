bd61ef5aaffa8c4fe100480a90655c474517f60a
dp <me@srcio.cn>
2022-11-29 10:01:40 +0800

    update gitee url

--- src/utils/configureGlobal.js
+++ src/utils/configureGlobal.js
@@ -1,7 +1,7 @@
 const configureGlobal = {
-  documentAddressDefault: 'https://gitee.com/wutong/demo-2048.git',
+  documentAddressDefault: 'https://gitee.com/wutong-paas/demo-2048.git',
   wutongDocumentAddress: 'https://www.wutong.com/',
-  documentAddress: 'https://gitee.com/wutong/',
+  documentAddress: 'https://gitee.com/wutong-paas/',
   newbieGuideShow: true,
   wutongTextShow: true
 };
