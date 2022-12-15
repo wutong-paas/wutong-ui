a9c661786227bf99343433e7a4aa312ff1e72e37
leon <liujinwen@talkweb.com.cn>
2022-12-15 15:40:14 +0800

    fix: replace v1 request url

--- src/services/toolkit.js
+++ src/services/toolkit.js
@@ -3,7 +3,7 @@ import request from '../utils/request';
 
 //获取团队列表
 export async function getTeamListAPI() {
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/teams`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/teams`, {
     method: 'get'
   });
 }
@@ -11,7 +11,7 @@ export async function getTeamListAPI() {
 //获取标签列表
 export async function getTagListAPI(body = {}) {
   const { start, end } = body;
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/log/labels`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/labels`, {
     method: 'get',
     params: {
       start: `${start}000000`,
@@ -24,7 +24,7 @@ export async function getTagListAPI(body = {}) {
 export async function getValueOfTagListAPI(body = {}) {
   const { label_id, start, end } = body;
   return request(
-    `${apiconfig.baseUrl}/console/wt-proxy/v1/log/label/${label_id}/values`,
+    `${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/label/${label_id}/values`,
     {
       method: 'get',
       params: {
@@ -38,7 +38,7 @@ export async function getValueOfTagListAPI(body = {}) {
 // 获取日志数据
 export async function getLogListAPI(body = {}) {
   const { expr, start, end } = body;
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/log/query`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/query`, {
     method: 'post',
     data: {
       expr,
@@ -51,7 +51,7 @@ export async function getLogListAPI(body = {}) {
 //获取调用链路列表接口
 export async function getTraceListAPI(body = {}) {
   const { minDuration, start, end, limit, tags } = body;
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/traces`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/traces`, {
     method: 'post',
     data: {
       minDuration,
@@ -65,14 +65,14 @@ export async function getTraceListAPI(body = {}) {
 
 //获取服务名称列表
 export async function getServicesListAPI() {
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/trace/services`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/trace/services`, {
     method: 'get'
   });
 }
 
 //获取链路节点名称列表
 export async function getComponentListAPI() {
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/trace/spans`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/trace/spans`, {
     method: 'get'
   });
 }
@@ -80,7 +80,7 @@ export async function getComponentListAPI() {
 //获取链路详情接口
 export async function getTraceDetailAPI(body = {}) {
   const { id, start, end } = body;
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/trace/${id}`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/trace/${id}`, {
     method: 'get',
     params: {
       start,
@@ -92,7 +92,7 @@ export async function getTraceDetailAPI(body = {}) {
 //获取标签和值合集
 export async function getTagAndValueList(body = {}) {
   const { start, end, match } = body;
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/log/label/series`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/label/series`, {
     method: 'get',
     params: {
       start: `${start}000000`,
@@ -106,7 +106,7 @@ export async function getTagAndValueList(body = {}) {
 export async function getLogRuleTemplateAPI(body = {}) {
   const { namespace } = body;
   return request(
-    `${apiconfig.baseUrl}/console/wt-proxy/v1/log/config/template`,
+    `${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/config/template`,
     {
       method: 'get',
       params: {
@@ -119,7 +119,7 @@ export async function getLogRuleTemplateAPI(body = {}) {
 // 获取日志提取配置
 export async function getLogRuleConfigAPI(body = {}) {
   const { namespace } = body;
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/log/config`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/config`, {
     method: 'get',
     params: {
       namespace
@@ -129,7 +129,7 @@ export async function getLogRuleConfigAPI(body = {}) {
 
 // 创建日志配置
 export async function createLogConfigAPI(body = {}) {
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/log/config`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/config`, {
     method: 'post',
     data: body
   });
@@ -137,7 +137,7 @@ export async function createLogConfigAPI(body = {}) {
 
 // 更新日志配置
 export async function updateLogConfigAPI(body = {}) {
-  return request(`${apiconfig.baseUrl}/console/wt-proxy/v1/log/config`, {
+  return request(`${apiconfig.baseUrl}/console/wt-proxy/obs/v1/log/config`, {
     method: 'put',
     data: body
   });
